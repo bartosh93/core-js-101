bf02b740f7be83a957191e5e82b9ff1ddb6dbffc
bartosh93 <bartosh93@yandex.ru>
2023-05-30 22:17:52 +0300

    init: start core-js-101 task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -19,8 +19,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 
@@ -35,8 +35,8 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return Math.PI * 2 * radius;
 }
 
 /**
@@ -51,8 +51,8 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return (((value1 / 100) + (value2 / 100)) / 2) * 100;
 }
 
 /**
@@ -70,8 +70,16 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const x = x1 > x2 ? x1 - x2 : x2 - x1;
+  const y = y1 > y2 ? y1 - y2 : y2 - y1;
+  let answer = 0;
+  if (x === 0 || y === 0) {
+    answer = Math.abs(x - y);
+  } else {
+    answer = Math.sqrt(x * x + y * y);
+  }
+  return answer;
 }
 
 /**
